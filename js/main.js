6b9f9be79bc145ddb8fc3a375c80140b11284e49
praveen <spraveenitpro@gmail.com>
2019-09-22 22:18:21 -0700

    Made the header navigation into a fixed top navigation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-
+const nav = document.querySelector("#page-nav");
+const topOfNav = nav.offsetTop;
+function fixNav() {
+	if (window.scrollY >= topOfNav) {
+		document.body.classList.add("fixed-nav");
+	} else {
+		document.body.classList.remove("fixed-nav");
+	}
+}
+window.addEventListener("scroll", fixNav);
