78970a7e4bd6059d00990658aef5be28b2c7ac9d
devraf <rafael.perales@outlook.com>
2019-09-28 19:40:26 -0500

    added slide animation
    updated js for endless slides animation
    
    added css for slide elements
    
    add slider container and slides html elements

--- js/main.js
+++ js/main.js
@@ -1,41 +1,69 @@
 // FIXED NAV
 
-const nav = document.querySelector("#page-nav");
-const topOfNav = nav.offsetTop;
+const nav = document.querySelector('#page-nav')
+const topOfNav = nav.offsetTop
 function fixNav() {
-	if (window.scrollY >= topOfNav) {
-		document.body.classList.add("fixed-nav");
-		document.getElementById("slider").style["padding-top"] = "45px"; //changed from 80 to 45
-	} else {
-		document.body.classList.remove("fixed-nav");
-	}
+  if (window.scrollY >= topOfNav) {
+    document.body.classList.add('fixed-nav')
+    document.getElementById('hero').style['padding-top'] = '44px' //changed from 80 to 45
+  } else {
+    document.body.classList.remove('fixed-nav')
+  }
 }
-window.addEventListener("scroll", fixNav);
+window.addEventListener('scroll', fixNav)
 
 // SLIDE SHOW
 
-var i = 0; // Start point for time
-var images = [];
-var time = 3000;
+//var i = 0; // Start point for time
+// var images = [];
+// var time = 3000;
 
 // Image list
 
-images[0] = "images/eating-area.jpg";
-images[1] = "images/dinner-table.jpg";
-images[2] = "images/meal-dinner-table.jpg";
-images[3] = "images/overhead-view-dinning-room.jpg";
+// images[0] = "images/eating-area.jpg";
+// images[1] = "images/dinner-table.jpg";
+// images[2] = "images/meal-dinner-table.jpg";
+// images[3] = "images/overhead-view-dinning-room.jpg";
 
 // Function to change image slide
 
-function changeImg() {
-	document.slide.src = images[i];
+// function changeImg() {
+// 	document.slide.src = images[i];
 
-	if (i < images.length - 1) {
-		i++;
-	} else {
-		i = 0;
-	}
-	setTimeout("changeImg()", time);
+// 	if (i < images.length - 1) {
+// 		i++;
+// 	} else {
+// 		i = 0;
+// 	}
+// 	setTimeout("changeImg()", time);
+// }
+
+// window.onload = changeImg;
+
+//Select container with slides
+var slideContainer = document.querySelector('.slideContainer')
+
+function addAnimationEventToChild() {
+  slideContainer.children[0].addEventListener('animationend', cycleSlides)
 }
+//removes the first slide to end of the slide list after animation plays
+function cycleSlides() {
+  var firstSlide = slideContainer.children[0]
+  var secondSlide = slideContainer.children[1]
 
-window.onload = changeImg;
+  firstSlide.classList.remove('moveSlide')
+  secondSlide.classList.remove('moveSlide')
+
+  firstSlide.remove()
+  slideContainer.appendChild(firstSlide)
+
+  firstSlide.removeEventListener('animationend', cycleSlides)
+}
+// adds animations to first and second slide
+function doAnimation() {
+  slideContainer.children[0].classList.add('moveSlide')
+  slideContainer.children[1].classList.add('moveSlide')
+  addAnimationEventToChild()
+}
+//auto runs the slider
+window.setInterval(doAnimation, 6500)
